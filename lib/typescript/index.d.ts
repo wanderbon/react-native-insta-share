481e318895503d1e5355185e473788a2d7bc53a0
Evgeniy Kozhich <wanderbon95@gmail.com>
2021-05-25 12:14:36 +0300

    fix

--- lib/typescript/index.d.ts
+++ lib/typescript/index.d.ts
@@ -1,5 +1,5 @@
 declare type InstaShareType = {
-    share(type: string, mediaPath: string): Promise<number>;
+    share(uri: string): Promise<number>;
 };
 declare const _default: InstaShareType;
 export default _default;

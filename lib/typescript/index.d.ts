311d18ec17f1e880b9a86a1fcea0a4cd29d15d2f
Evgeniy Kozhich <wanderbon95@gmail.com>
2021-05-25 16:11:33 +0300

    feat:types

--- lib/typescript/index.d.ts
+++ lib/typescript/index.d.ts
@@ -1,5 +1,12 @@
+declare enum Errors {
+    APP_NOT_INSTALL = "app_not_install",
+    URL_NOT_VALID = "url_not_valid",
+    DATA_NOT_VALID = "data_not_valid",
+    PERMISSIONS_NOT_RECIVED = "permissions_not_recived"
+}
 declare type InstaShareType = {
     share(uri: string): Promise<number>;
 };
 declare const _default: InstaShareType;
 export default _default;
+export { Errors };
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,12 @@
 import { NativeModules } from 'react-native';
 
+enum Errors {
+  APP_NOT_INSTALL = 'app_not_install',
+  URL_NOT_VALID = 'url_not_valid',
+  DATA_NOT_VALID = 'data_not_valid',
+  PERMISSIONS_NOT_RECIVED = 'permissions_not_recived',
+}
+
 type InstaShareType = {
   share(uri: string): Promise<number>;
 };
@@ -7,3 +14,4 @@ type InstaShareType = {
 const { InstaShare } = NativeModules;
 
 export default InstaShare as InstaShareType;
+export { Errors };

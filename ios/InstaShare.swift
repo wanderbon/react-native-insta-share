619822204ae816a6b9cfb4462e72ae81eab27043
Evgeniy Kozhich <wanderbon95@gmail.com>
2021-05-25 15:34:12 +0300

    Update InstaShare.swift

--- ios/InstaShare.swift
+++ ios/InstaShare.swift
@@ -1,51 +1,58 @@
-@objc(InstaShare)
-class InstaShare: NSObject, UIDocumentInteractionControllerDelegate {
-      private let documentInteractionController = UIDocumentInteractionController()
-      private let kInstagramURL = "instagram://"
-      private let kUTI = "com.instagram.photo" //"com.instagram.exclusivegram"
-      private let kfileNameExtension = "instagram.ig"//"instagram.igo"
-      private let kAlertViewTitle = "Error"
-      private let kAlertViewMessage = "Please install the Instagram application"
-      
-      func postImageToInstagramWithCaption(imageInstagram: UIImage) {
-          let instagramURL = NSURL(string: kInstagramURL)
-          if UIApplication.shared.canOpenURL(instagramURL! as URL) {
-              let jpgPath = (NSTemporaryDirectory() as NSString).appendingPathComponent(kfileNameExtension)
-              
-              do {
-              try imageInstagram.jpegData(compressionQuality: 1)?.write(to: URL(fileURLWithPath: jpgPath), options: .atomic)
-              } catch {
-                  print(error)
-              }
+import Photos
 
-              let rect = CGRect.zero
-              let fileURL = NSURL.fileURL(withPath: jpgPath)
-              
-              
-              documentInteractionController.url = fileURL
-              documentInteractionController.delegate = self
-              documentInteractionController.uti = kUTI
-              
-              let instagramCaption = ""
-            
-              // adding caption for the image
-              documentInteractionController.annotation = ["InstagramCaption": instagramCaption]
-//              documentInteractionController.presentOpenInMenu(from: rect, in: view, animated: true)
-          }
-          else {
-              
-              // alert displayed when the instagram application is not available in the device
-              UIAlertView(title: kAlertViewTitle, message: kAlertViewMessage, delegate:nil, cancelButtonTitle:"Ok").show()
-          }
-      }
-    
-    @objc(share:withResolver:withRejecter:)
-    func share(uri: String, resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) -> Void {
-        if NSFileManager.defaultManager().fileExistsAtPath(uri) {
-            let url = NSURL(string: imageUrlPath)
-            let data = NSData(contentsOfURL: url!)
-            
-            postImageToInstagramWithCaption(imageInstagram: UIImage(data: data!))
+@objc(InstaShare)
+class InstaShare: NSObject {
+    private let kInstagramURL = "instagram://app"
+    private let kAlertViewTitle = "error"
+    private let kAlertViewMessage = "app_not_install"
+
+    func postImageToInstagramWithCaption(imageInstagram: UIImage, resolve: @escaping RCTPromiseResolveBlock, reject: @escaping RCTPromiseRejectBlock) {
+        let instagramURL = NSURL(string: kInstagramURL)
+        
+        if UIApplication.shared.canOpenURL(instagramURL! as URL) {
+            do {
+                try PHPhotoLibrary.shared().performChangesAndWait {
+                    let imgReq = PHAssetChangeRequest.creationRequestForAsset(from: imageInstagram)
+                    let localIdentifier = imgReq.placeholderForCreatedAsset?.localIdentifier
+                    let u = "instagram://library?LocalIdentifier=" + localIdentifier!
+                    
+                    UIApplication.shared.open(NSURL(string: u)! as URL, options: [:], completionHandler: nil)
+                 }
+            } catch {
+                let errorTemp = NSError(domain:"", code:104, userInfo:nil)
+                reject(self.kAlertViewTitle, "permissions_not_recived", errorTemp)
+            }
         }
+        else {
+            let stringURL = "http://itunes.apple.com/app/instagram/id389801252"
+            let url = URL(string: stringURL)
+            
+            if let url = url {
+                UIApplication.shared.open(url, options: [:], completionHandler: nil)
+            }
+            
+            let errorTemp = NSError(domain:"", code:101, userInfo:nil)
+            reject(kAlertViewTitle, kAlertViewMessage, errorTemp)
+        }
+    }
+
+    @objc(share:withResolver:withRejecter:)
+    func share(uri: String, resolve: @escaping RCTPromiseResolveBlock,reject: @escaping RCTPromiseRejectBlock) -> Void {
+        guard let backgroundImageURL = RCTConvert.nsurl(uri) else {
+            let errorTemp = NSError(domain:"", code:102, userInfo:nil)
+            return reject(kAlertViewTitle, "url_not_valid", errorTemp)
+        }
+        
+        var image: UIImage? = nil
+
+        do {
+            let data = try Data(contentsOf: backgroundImageURL)
+            image = UIImage(data: data)
+        } catch {
+            let errorTemp = NSError(domain:"", code:103, userInfo:nil)
+            reject(kAlertViewTitle, "data_not_valid", errorTemp)
+        }
+
+        postImageToInstagramWithCaption(imageInstagram: image!, resolve: resolve, reject: reject)
     }
 }

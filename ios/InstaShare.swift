4e4a18e3c682c04c2b2b078a1aa7d4975001df84
Evgeniy Kozhich <wanderbon95@gmail.com>
2021-05-26 17:37:33 +0300

    switch for ios less 13

--- ios/InstaShare.swift
+++ ios/InstaShare.swift
@@ -1,8 +1,13 @@
+import Foundation
+import UIKit
 import Photos
 
 @objc(InstaShare)
-class InstaShare: NSObject {
-    private let kInstagramURL = "instagram://app"
+class InstaShare: NSObject, UIDocumentInteractionControllerDelegate {
+    private let documentInteractionController = UIDocumentInteractionController()
+    private let kInstagramURL = "instagram://"
+    private let kUTI = "com.instagram.exclusivegram" // "com.instagram.photo"
+    private let kfileNameExtension = "instagram.igo" // "instagram.ig"
     private let kAlertViewTitle = "101"
     private let kAlertViewMessage = "app_not_install"
 
@@ -10,17 +15,43 @@ class InstaShare: NSObject {
         let instagramURL = NSURL(string: kInstagramURL)
         
         if UIApplication.shared.canOpenURL(instagramURL! as URL) {
-            do {
-                try PHPhotoLibrary.shared().performChangesAndWait {
-                    let imgReq = PHAssetChangeRequest.creationRequestForAsset(from: imageInstagram)
-                    let localIdentifier = imgReq.placeholderForCreatedAsset?.localIdentifier
-                    let u = "instagram://library?LocalIdentifier=" + localIdentifier!
-                    
-                    UIApplication.shared.open(NSURL(string: u)! as URL, options: [:], completionHandler: nil)
-                 }
-            } catch {
-                let errorTemp = NSError(domain:"", code:104, userInfo:nil)
-                reject(self.kAlertViewTitle, "permissions_not_recived", errorTemp)
+            if #available(iOS 13, *) {
+                do {
+                    try PHPhotoLibrary.shared().performChangesAndWait {
+                        let imgReq = PHAssetChangeRequest.creationRequestForAsset(from: imageInstagram)
+                        let localIdentifier = imgReq.placeholderForCreatedAsset?.localIdentifier
+                        let u = "instagram://library?LocalIdentifier=" + localIdentifier!
+                        
+                        UIApplication.shared.open(NSURL(string: u)! as URL, options: [:], completionHandler: nil)
+                     }
+                } catch {
+                    let errorTemp = NSError(domain:"", code:104, userInfo:nil)
+                    reject(self.kAlertViewTitle, "permissions_not_recived", errorTemp)
+                }
+            } else {
+                let jpgPath = (NSTemporaryDirectory() as NSString).appendingPathComponent(kfileNameExtension)
+                            
+                do {
+                    try imageInstagram.jpegData(compressionQuality: 0.75)?.write(to: URL(fileURLWithPath: jpgPath), options: .atomic)
+                } catch {
+                    let errorTemp = NSError(domain:"", code:102, userInfo:nil)
+                    reject(kAlertViewTitle, "url_not_valid", errorTemp)
+                }
+
+                let rect = CGRect.zero
+                let fileURL = NSURL.fileURL(withPath: jpgPath)
+                
+                documentInteractionController.url = fileURL
+                documentInteractionController.delegate = self
+                documentInteractionController.uti = kUTI
+
+                documentInteractionController.annotation = ["InstagramCaption": "Share to"]
+                let rootView = UIApplication.shared.delegate?.window??.rootViewController?.view
+                
+                DispatchQueue.main.async {
+                    self.documentInteractionController.presentOpenInMenu(from: rect, in: rootView!, animated: true)
+                }
+                //
             }
         }
         else {
